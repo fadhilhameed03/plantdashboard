b1ed05db4990a283a5d6d82a842b4b882632ac95
FABBIO\fadhi <fadhilhameed2004@gmail.com>
2023-11-04 00:07:14 +0530

    90%

--- src/Components/Body Section/Activity Section/Activity.jsx
+++ src/Components/Body Section/Activity Section/Activity.jsx
@@ -3,6 +3,12 @@ import './Activity.css'
 
 //Imported Images ===========>
 import user1 from '../../../Assets/user1.jpg'
+import user2 from '../../../Assets/user2.jpg'
+import user5 from '../../../Assets/user5.jpg'
+import user6 from '../../../Assets/user6.jpg'
+import user7 from '../../../Assets/user7.jpg'
+import user8 from '../../../Assets/user8.jpg'
+import user9 from '../../../Assets/user9.jpg'
 
 //Imported Icons ============>
 import {BsArrowRightShort} from 'react-icons/bs'
@@ -12,15 +18,16 @@ const Activity = () => {
   return (
     <div className='activitySection'>
       <div className="heading flex">
-        <h1>Resent Activity</h1>
+        <h1>Recent Activity</h1>
         <button className='btn flex'>
           See All
           <BsArrowRightShort className='icon'/>
         </button>
       </div>
+
       <div className="secContainer grid">
         <div className="singleCustomer flex">
-          <img src={user1} alt='customer image'/>
+          <img src={user5} alt='customer image'/>
           <div className="customerDetails">
             <span className="name">
               Alexa
@@ -28,6 +35,94 @@ const Activity = () => {
             <small>
               Ordered a new plant
             </small>
+          </div>
+          <div className="duration">
+            2 min ago 
+          </div>
+        </div>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user6} alt='customer image'/>
+          <div className="customerDetails">
+            <span className="name">
+              Ami
+            </span>
+            <small>
+              Ordered a new plant
+            </small>
+          </div>
+          <div className="duration">
+            4 min ago 
+          </div>
+        </div>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user7} alt='customer image'/>
+          <div className="customerDetails">
+            <span className="name">
+              Ella
+            </span>
+            <small>
+              Ordered a new plant
+            </small>
+          </div>
+          <div className="duration">
+            7 min ago 
+          </div>
+        </div>
+      </div>
+      
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user8} alt='customer image'/>
+          <div className="customerDetails">
+            <span className="name">
+              Alice
+            </span>
+            <small>
+              Ordered a new plant
+            </small>
+          </div>
+          <div className="duration">
+            9 min ago 
+          </div>
+        </div>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user9} alt='customer image'/>
+          <div className="customerDetails">
+            <span className="name">
+              Grace
+            </span>
+            <small>
+              Ordered a new plant
+            </small>
+          </div>
+          <div className="duration">
+            18 min ago 
+          </div>
+        </div>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user1} alt='customer image'/>
+          <div className="customerDetails">
+            <span className="name">
+              Ava
+            </span>
+            <small>
+              Ordered a new plant
+            </small>
+          </div>
+          <div className="duration">
+            24 min ago 
           </div>
         </div>
       </div>
